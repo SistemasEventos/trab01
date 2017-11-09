a14019d6523aed6459c11b35ae825535510241cf
HaraHeique <heikacademicos@hotmail.com>
2017-11-09 21:33:45 -0200

    Imagens e script com operador like do projeto SE

--- SQL/ModeloFisico_selects_like.sql
+++ SQL/ModeloFisico_selects_like.sql
@@ -2,11 +2,12 @@
 SELECT nome, userlogin FROM cliente WHERE nome LIKE 'C%';
 SELECT nome, userlogin FROM cliente WHERE userlogin ILIKE '%t%';
 SELECT nome, userlogin, iduser FROM cliente WHERE userlogin LIKE '______' AND nome ILIKE '%a';
-SELECT nome, fk_premium_fk_cliente_iduser AS "Cliente Premium Responsável" FROM evento
+SELECT nome AS "Nome do evento", fk_premium_fk_cliente_iduser AS "ID Cliente Premium Responsável" FROM evento
 WHERE nome LIKE 'A%' AND nome NOT ILIKE '%S';
-SELECT descricao AS "gmail", fk_cliente_iduser AS "ID Cliente" FROM contatos_cliente_tem 
-WHERE fk_contato_idtipocontato = 3 AND descricao LIKE '%@gmail%';
-SELECT descricao AS "hotmail ou outlook", fk_pontovenda_idpontovenda AS "ID Ponto de Venda" FROM contatos_pontosvenda_tem
-WHERE fk_contato_idtipocontato = 3 AND descricao ILIKE '%@hotmail%' OR descricao ILIKE '%@outlook%';
+SELECT descricao AS "gmail", fk_cliente_iduser AS "ID Cliente" FROM cliente_contato
+WHERE fk_tipocontato_idtipocontato = 3 AND descricao LIKE '%@gmail%';
+SELECT descricao AS "hotmail ou outlook", fk_pontovenda_idpontovenda AS "ID Ponto de Venda" FROM pontovenda_contato
+WHERE fk_tipocontato_idtipocontato = 3 AND descricao ILIKE '%@hotmail%' OR descricao ILIKE '%@outlook%';
+
 
 
